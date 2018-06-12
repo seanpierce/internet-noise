04d08e18f1251d6aa2cbc2b560443a4fd934225e
Sean Pierce Sumler <ssumler@i-ology.com>
2018-06-12 07:41:40 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
-var noiseWindow;
+var noiseWindow
 
-function startNoise() {
-  noiseWindow = window.open('https://www.google.com/search?btnI&q=filetype:html+cats');
-  function change_noise() {
-    let a = nouns[Math.floor(Math.random()*nouns.length)];
-    let b = nouns[Math.floor(Math.random()*nouns.length)];
-    let url = "https://www.google.com/search?btnI&q=filetype:html+" + a + " " + b;
-    noiseWindow.location.href = url;
+startNoise = () => {
+  noiseWindow = window.open('https://www.google.com/search?btnI&q=filetype:html+cats')
+  change_noise = () => {
+    let noun1 = nouns[Math.floor(Math.random() * nouns.length)]
+    let noun2 = nouns[Math.floor(Math.random() * nouns.length)]
+    let url = "https://www.google.com/search?btnI&q=filetype:html+" + noun1 + " " + noun2
+    noiseWindow.location.href = url
     setTimeout(() => {
-      change_noise();
-    }, 8000);
-  };
-  change_noise();
-};
-
-function stopNoise() {
-  noise_win.close();
+      change_noise()
+    }, 8000)
+  }
+  change_noise()
 }
 
-document.getElementById('start').addEventListener("click", startNoise);
-document.getElementById('stop').addEventListener("click", stopNoise);
+stopNoise = () => {
+  noise_win.close()
+}
+
+document.getElementById('start').addEventListener("click", startNoise)
+document.getElementById('stop').addEventListener("click", stopNoise)
